e5c83274c5cec6731c5c9c1cdbcec0ba0a68d147
Lepi Czarny <michal.lepczynski@gmail.com>
2019-07-01 00:23:37 +0200

    experiment with wrapping the code with a class rather than a function

--- main.py
+++ main.py
@@ -24,58 +24,62 @@ H = HEIGHT -1
 
 
 
+class BusinessCard(object):
 
-def loop(delay=5):
+    def __init__(self, delay=5):
+        self.delay = delay
+        self.bc_text = [] # [ 0 for x in range(4) ]
+        self.bc_off = []  # [ 0 for x in range(4) ]
 
-    bc_text = [None] # [ 0 for x in range(4) ]
-    bc_off = [None]  # [ 0 for x in range(4) ]
+        self.loadBusinessCard()
+        self.drawBusinessCard()
 
 
-    def loadBusinessCard(filename='properties.business.card.txt'):
+    def loadBusinessCard(self, filename='properties.business.card.txt'):
         print('loading business card from a config file...')
         
         try:
-            bc_text = [ x.rstrip() for x in open(filename, 'r').readlines() ]
+            self.bc_text = [ x.rstrip() for x in open(filename, 'r').readlines() ]
         except:
             print('failed to read business card config, loading defaults...')
-            bc_text = ['Name', 'Surname', '-------', 'Hello ;)']
+            self.bc_text = ['Name', 'Surname', '-------', 'Hello ;)']
         finally:
-            print('business card text: ', bc_text)
+            print('business card text: ', self.bc_text)
 
-        return bc_text
+        self.calculateBusinessCardIndents()
 
 
-    def calculateBusinessCardIndents():
+    def calculateBusinessCardIndents(self):
         import math as m
 
-        print('Calculating business card indents from bc_text: ', bc_text)
+        print('Calculating business card indents from bc_text: ', self.bc_text)
 
-        bc_off = [ len(x) for x in bc_text ]
-        print('business card item lengths: ', bc_off)
+        self.bc_off = [ len(x) for x in self.bc_text ]
+        print('business card item lengths: ', self.bc_off)
         
-        bc_off = [ m.ceil( 8 -(x/2) ) for x in bc_off]
+        self.bc_off = [ m.ceil( 8 -(x/2) ) for x in self.bc_off]
         print('business card item offsets in number of chars: ', bc_off)
         
         #transforms the list of indent numbers for center allignment into space string of such lengths
-        bc_off = [ ( "".join(map(str, [ " " for i in range(x) ] )) ) for x in bc_off ]
-        print('business card item offsets in space strings: ', bc_off)
+        self.bc_off = [ ( "".join(map(str, [ " " for i in range(x) ] )) ) for x in self.bc_off ]
+        print('business card item offsets in space strings: ', self.bc_off)
 
 
-    def drawBusinessCard():
+    def drawBusinessCard(self):
         # toDo: devise a better idea than wiping it all out
         # and maybe draw text in a 1x1 pixel cross pattern in black first
         # oled.fill(0)
 
-        print("bc_off: ", bc_off)
-        print("bc_text: ", bc_text)
+        print("bc_off: ", self.bc_off)
+        print("bc_text: ", self.bc_text)
 
-        oled.text(bc_off[2] + bc_text[2], 0, 17)
-        oled.text(bc_off[0] + bc_text[0], 0, 0)
-        oled.text(bc_off[1] + bc_text[1], 0, 9)
-        oled.text(bc_off[3] + bc_text[3], 4, 24)
+        oled.text(self.bc_off[2] + self.bc_text[2], 0, 17)
+        oled.text(self.bc_off[0] + self.bc_text[0], 0, 0)
+        oled.text(self.bc_off[1] + self.bc_text[1], 0, 9)
+        oled.text(self.bc_off[3] + self.bc_text[3], 4, 24)
 
 
-    def drawCorners(color=1, length=3): #length and color
+    def drawCorners(self, color=1, length=3): #length and color
         oled.hline( 0        , 0        ,length,color)  #  F  yest
         oled.vline( 0        , 0        ,length,color)
 
@@ -88,36 +92,33 @@ def loop(delay=5):
         oled.hline( 0        , H        ,length,color)  #  length  yest
         oled.vline( 0        , H-length ,length,color)
 
-    bc_text = loadBusinessCard()
-    
-    drawBusinessCard()
 
-    import utime
-    i = 0
-    corners_last = ants_loop = utime.ticks_ms()
-    time4on = False
+    def loop(self):
+        import utime
+        corners_last = ants_loop = utime.ticks_ms()
+        time4on = False
 
 
-    while True:
-        now = utime.ticks_ms()
+        while True:
+            now = utime.ticks_ms()
 
-        if utime.ticks_diff(now, corners_last) > corners_interval_off and not time4on:
-            drawCorners(1)
-            time4on = True
-        if utime.ticks_diff(now, corners_last) > corners_interval_off + corners_interval_on:
-            drawCorners(0)
+            if utime.ticks_diff(now, corners_last) > corners_interval_off and not time4on:
+                drawCorners(1)
+                time4on = True
+            if utime.ticks_diff(now, corners_last) > corners_interval_off + corners_interval_on:
+                drawCorners(0)
+                drawBusinessCard()
+                corners_last = now
+                time4on = False
+
+            if utime.ticks_diff(now, ants_loop) > ants_loop_length_ms:
+                ants_loop = now
+
+            print(utime.ticks_diff(now, ants_loop))
+
             drawBusinessCard()
-            corners_last = now
-            time4on = False
-
-        if utime.ticks_diff(now, ants_loop) > ants_loop_length_ms:
-            ants_loop = now
-
-        print(utime.ticks_diff(now, ants_loop))
-
-        drawBusinessCard()
-        oled.show()
+            oled.show()
 
 
 if __name__ == '__main__':
-    loop()
+    BusinessCard().loop()
